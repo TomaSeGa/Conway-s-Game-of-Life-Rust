5c13214db656012190a462203dd876baa801abae
Thomas SEGARD <segardthomas78@gmail.com>
2024-04-23 21:50:57 +0200

    First test with macroquad and cell.rs creation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Game of Life")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        //draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 120.0, 20.0, 20.0, WHITE);
+        //draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        draw_text("Conway's Game of Life in Rust", 20.0, 20.0, 30.0, DARKGRAY);
+        
+        next_frame().await
+    }
 }
+
